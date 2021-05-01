6a96631d254e1c5c0236f2b2faa300e34129f463
FabriceT <fabrice.tsiava@artik-consulting.com>
2021-05-01 16:48:49 +0200

    renaming

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import p5, { Color, Renderer, Vector } from "p5";
-import { physicsSketch } from "./mover/mover";
+import { physicsSketch } from "./physics/physics";
 import { triangleSketch } from "./triangles/triangles";
 
 // new p5(triangleSketch, document.body);
