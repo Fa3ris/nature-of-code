2447e512244ab3f2c35aa9dcf1f977609c59886a
Fa3ris <Fa3ris@users.noreply.github.com>
2021-05-06 15:54:43 +0200

    roundabout

---
+++ src/circle/roundabout.ts
@@ -0,0 +1,45 @@
+import { Queue } from "mnemonist";
+import p5, { Color, Vector } from "p5";
+import { Mover } from "../mover/mover";
+
+export const roundabout = (p5: p5) => {
+  const width = 711;
+  const height = 400;
+
+  const inc = 0.01;
+  let phasing = 0;
+
+  const baseRadius = 30;
+  let r = baseRadius;
+
+  const variation = 5;
+
+  p5.setup = () => {
+    p5.createCanvas(width, height);
+
+    p5.noStroke();
+  };
+
+  p5.draw = () => {
+    p5.background(100);
+    p5.push();
+    p5.translate(width / 2, height / 2);
+    for (let i = 0; i < p5.TAU; i += p5.QUARTER_PI) {
+      p5.fill(52, 164, 235, 255 - i * 50);
+      p5.circle(r * p5.cos(i + phasing), r * p5.sin(i + phasing), r / 2);
+    }
+    p5.pop();
+
+    phasing -= inc;
+    phasing %= p5.TAU;
+
+    r = baseRadius + variation * p5.sin(phasing);
+  };
+};
+
+// class Round {
+
+//     private theta: number;
+//     private color: Color
+
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import p5 from "p5";
+import { roundabout } from "./circle/roundabout";
 import { curve } from "./curve/curve";
 import { angularMotion } from "./physics/angular-motion";
 import { physicsSketch2 } from "./physics/physics2";
@@ -11,5 +12,6 @@ document.body.append(container);
 // new p5(triangleSketch, container);
 // new p5(physicsSketch);
 // new p5(physicsSketch2, container);
-new p5(angularMotion, container);
+// new p5(angularMotion, container);
+new p5(roundabout, container);
 // new p5(curve);
